61f709ca74609d8e68a1ae80f6ad90057224de05
Duong Minh Hieu <hieutkhd03@gmail.com>
2024-12-12 10:26:12 +0700

    feature/login-with-role-admin

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,58 +2,49 @@ import axios from "@/http";
 
 // Đăng nhập
 export const login = (email, password) => {
-  // eslint-disable-next-line no-undef
-  return (
-    axios
-      // eslint-disable-next-line no-undef
-      .post("login", { email, password })
-      .then((response) => {
-        console.log("response: ", response);
-        if (response?.success) {
-          // Lưu thông tin người dùng vào localStorage nếu success
-          const userData = response.data;
-          localStorage.setItem("user", JSON.stringify(userData));
-          return userData; // Trả về thông tin người dùng
-        }
-        throw response.error; // Ném lỗi từ backend để frontend xử lý
-      })
-  );
+  return axios.post("login", { email, password }).then((response) => {
+    console.log("response: ", response);
+    if (response?.success) {
+      // Lưu thông tin người dùng vào localStorage nếu success
+      const userData = response.data;
+      localStorage.setItem("user", JSON.stringify(userData));
+      return userData; // Trả về thông tin người dùng
+    }
+    throw response.error; // Ném lỗi từ backend để frontend xử lý
+  });
 };
 
 // Đăng ký
 export const register = (email, password, fullName, address, phone) => {
-  return (
-    axios
-      // eslint-disable-next-line no-undef
-      .post("register", {
-        email,
-        password,
-        fullName,
-        address,
-        phone,
-      }) // Gửi dữ liệu đăng ký
-      .then((response) => {
-        if (response?.success) {
-          return response.data; // Trả về dữ liệu phản hồi từ server nếu success là true
-        } else {
-          throw response.error; // Ném lỗi nếu success là false
-        }
-      })
-  );
+  return axios
+    .post("register", {
+      email,
+      password,
+      fullName,
+      address,
+      phone,
+    })
+    .then((response) => {
+      if (response?.success) {
+        return response.data; // Trả về dữ liệu phản hồi từ server nếu success là true
+      } else {
+        throw response.error; // Ném lỗi nếu success là false
+      }
+    });
 };
 
-// Đăng xuất: Xóa thông tin người dùng khỏi localStorage
+// Đăng xuất
 export const logout = () => {
   localStorage.removeItem("user"); // Xóa thông tin người dùng trong localStorage
 };
 
-// Lấy thông tin người dùng hiện tại từ localStorage
+// Lấy thông tin người dùng hiện tại
 export const getCurrentUser = () => {
   const user = localStorage.getItem("user"); // Lấy dữ liệu từ localStorage
   return user ? JSON.parse(user) : null;
 };
 
-// Lấy vai trò người dùng hiện tại từ thông tin BE trả về
+// Lấy vai trò người dùng hiện tại
 export const getCurrentUserRole = () => {
   const user = this.getCurrentUser();
   return user ? user.role : null; // Trả về role nếu tồn tại
